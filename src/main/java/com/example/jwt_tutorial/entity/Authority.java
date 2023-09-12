71c6ba802b8703dd1e1fa02f6d98d643e7b58180
minkyung <minkyung7365@gmail.com>
2023-09-12 12:40:50 +0900

    [DELETE]2_복습

--- src/main/java/com/example/jwt_tutorial/entity/Authority.java
+++ src/main/java/com/example/jwt_tutorial/entity/Authority.java
@@ -2,7 +2,10 @@ package com.example.jwt_tutorial.entity;
 
 import lombok.*;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "authority")
